bbeab4302925d108ae065f01db8ebc5cd62d916f
MrSpunki <spunki994@gmail.com>
2018-02-21 13:23:37 +0300

    search for love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  
+  var loveTriangle = 0;
+
+  for (var i = 0; i < preferences.length; i++) {
+    var n = preferences[i];
+    if ((n + 1) == preferences[i + 1]){
+      if ((n -1) == preferences[i + 2]){
+        loveTriangle++;
+      }
+    }
+  }
+  return loveTriangle;
 };
